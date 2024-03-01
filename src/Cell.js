4498db6e12f76f7965c5f172af331b8538f1db9f
Caleb Chiang <calebchiang1002@gmail.com>
2024-03-01 13:08:38 -0800

    Updated styles to bold lines to look more like sudoku board

--- src/Cell.js
+++ src/Cell.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './styles/Cell.css';
 
 function Cell({value, onChange, rowIndex, colIndex}){
+    const isRightBorder = (colIndex + 1) % 3 === 0 && colIndex !== 8;
+    const isBottomBorder = (rowIndex + 1) % 3 === 0 && rowIndex !== 8;
+
+    let cellClass = 'cell';
+    if (isRightBorder && isBottomBorder) {
+        cellClass += ' border-bottom-right';
+    } else if (isRightBorder) {
+        cellClass += ' border-right';
+    } else if (isBottomBorder) {
+        cellClass += ' border-bottom';
+    }
+
     return (
-        <input className = 'cell'
+        <input className ={cellClass}
             type = "number"
             value = {value}
             onChange = {onChange}
